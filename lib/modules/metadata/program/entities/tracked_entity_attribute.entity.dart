475092b126b2897d17bf8bd3941de927f5f09171
Hamza Assada <7amza.it@gmail.com>
2023-03-06 21:24:43 +0300

    add valueType to entity

--- lib/modules/metadata/program/entities/tracked_entity_attribute.entity.dart
+++ lib/modules/metadata/program/entities/tracked_entity_attribute.entity.dart
@@ -6,6 +6,61 @@ import 'package:d2_remote/shared/entities/identifiable.entity.dart';
     tableName: 'trackedentityattribute',
     apiResourceName: 'trackedEntityAttributes')
 class TrackedEntityAttribute extends IdentifiableEntity {
-  TrackedEntityAttribute({required String id, required bool dirty})
-      : super(id: id, dirty: dirty);
+
+  @Column(type: ColumnType.TEXT, length: 50)
+  String valueType;
+
+  @Column(type: ColumnType.TEXT, nullable: true)
+  String? description;
+
+  // NMCP Ext
+  TrackedEntityAttribute(
+      {required String id,
+        String? created,
+        String? lastUpdated,
+        required String name,
+        required String shortName,
+        String? code,
+        String? displayName,
+        this.description,
+        required this.valueType,
+        required dirty})
+      : super(
+      id: id,
+      name: name,
+      shortName: shortName,
+      displayName: displayName,
+      code: code,
+      created: created,
+      lastUpdated: lastUpdated,
+      dirty: dirty);
+
+  factory TrackedEntityAttribute.fromJson(Map<String, dynamic> json) {
+    return TrackedEntityAttribute(
+        id: json['id'],
+        name: json['name'],
+        created: json['created'],
+        shortName: json['shortName'],
+        code: json['code'],
+        displayName: json['displayName'],
+        valueType: json['valueType'],
+        description: json['description'],
+        dirty: json['dirty']);
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['lastUpdated'] = this.lastUpdated;
+    data['id'] = this.id;
+    data['created'] = this.created;
+    data['name'] = this.name;
+    data['shortName'] = this.shortName;
+    data['code'] = this.code;
+    data['displayName'] = this.displayName;
+    data['valueType'] = this.valueType;
+    data['description'] = this.description;
+    data['dirty'] = this.dirty;
+
+    return data;
+  }
 }
