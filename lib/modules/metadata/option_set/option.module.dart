93a73621585d52b87cf5c13c25f8528814e22b92
Hamza Assada <7amza.it@gmail.com>
2023-03-07 23:00:27 +0300

    add OGroup

--- lib/modules/metadata/option_set/option.module.dart
+++ lib/modules/metadata/option_set/option.module.dart
@@ -1,14 +1,18 @@
 import 'package:d2_remote/modules/metadata/option_set/queries/option.query.dart';
+import 'package:d2_remote/modules/metadata/option_set/queries/option_group.query.dart';
 import 'package:d2_remote/modules/metadata/option_set/queries/option_set.query.dart';
 
 // replaced OptionSetModule as the parent module
 class OptionModule {
   static createTables() async {
+    await OptionGroupQuery().createTable();
     await OptionSetQuery().createTable();
     await OptionQuery().createTable();
   }
 
   OptionSetQuery get optionSet => OptionSetQuery();
+
+  OptionGroupQuery get optionGroup => OptionGroupQuery();
 
   OptionQuery get option => OptionQuery();
 }
