86aaa565dbcad491c99786a327e13251424debae
Hamza Assada <7amza.it@gmail.com>
2023-03-10 18:20:22 +0300

    add UidsHelper

---
+++ lib/core/arch/helpers/uids_helper.dart
@@ -0,0 +1,29 @@
+import 'package:d2_remote/shared/entities/identifiable.entity.dart';
+
+abstract class UidsHelper {
+  /// Returns a [Set] of uids of the given objects.
+  ///
+  /// @param objects A collection of objects with uid.
+  /// @return A [Set] with the uids of the given objects.
+  static Set<String> getUids<T extends IdentifiableEntity>(
+      [Iterable<T> objects = const []]) {
+    return getUidsList(objects).toSet();
+  }
+
+  /// Returns a [List] of uids of the given objects.
+  ///
+  /// @param objects A collection of objects with uid.
+  /// @return A [List] with the uids of the given objects.
+  static List<String> getUidsList<T extends IdentifiableEntity>(
+      [Iterable<T> objects = const []]) {
+    return objects.map((e) => e.id!).toList();
+  }
+
+  /// Return the uid of the object if the object is not null. If it is null, return null.
+  ///
+  /// @param o A object with uid.
+  /// @return The uid of the object.
+  static String? getUidOrNull(IdentifiableEntity? o) {
+    return o?.id;
+  }
+}
