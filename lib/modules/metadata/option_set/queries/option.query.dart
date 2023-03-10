e55da0aba9cca0643556162916a9f53bc7870a92
Hamza Assada <7amza.it@gmail.com>
2023-03-10 17:49:38 +0300

    Fix OptionQuery type

--- lib/modules/metadata/option_set/queries/option.query.dart
+++ lib/modules/metadata/option_set/queries/option.query.dart
@@ -1,8 +1,8 @@
-import 'package:d2_remote/modules/metadata/option_set/entities/option_set.entity.dart';
+import 'package:d2_remote/modules/metadata/option_set/entities/option.entity.dart';
 import 'package:d2_remote/shared/queries/base.query.dart';
 import 'package:sqflite/sqflite.dart';
 
-class OptionQuery extends BaseQuery<OptionSet> {
+class OptionQuery extends BaseQuery<Option> {
   OptionQuery({Database? database}) : super(database: database);
   String? optionSet;
 
