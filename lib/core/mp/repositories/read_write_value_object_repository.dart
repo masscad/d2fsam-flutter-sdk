7efab0eafb01e8b5a0527aaded9edf7e0b6a345d
Hamza Assada <7amza.it@gmail.com>
2023-03-14 21:07:45 +0300

    fix the import uri

--- lib/core/mp/repositories/read_write_value_object_repository.dart
+++ lib/core/mp/repositories/read_write_value_object_repository.dart
@@ -1,4 +1,4 @@
-import 'package:d2_remote/core/MP/repositories/read_write_object_repository.dart';
+import 'package:d2_remote/core/mp/repositories/read_write_object_repository.dart';
 import 'package:d2_remote/shared/entities/base.entity.dart';
 
 abstract class ReadWriteValueObjectRepository<M extends BaseEntity>
